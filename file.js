b08cde1e99a2c440b09e068b3f1fe808b35293c9
kashkostwebsites <155662960+kashkostwebsites@users.noreply.github.com>
2024-05-29 15:19:14 +0100

    file

--- file.js
+++ file.js
@@ -1,10 +1,12 @@
 const fs = require('fs');
 
 
-fs.appendFile('uifile1.ui', '<!-- Write some HTML code, We style it, No doctype html. -->',   (err)); {
+fs.appendFile('uifile1.ui', '<!-- Write some HTML code, We style it, No doctype html. -->',   (err, other)); {
 if (err) {
   console.log('Failed adding UI file')
 }
 
+console.log(other)
 
 } 
+
