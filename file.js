706ada3521dae0398f9ac0e2034d4eefe11df3d9
kashkostwebsites <155662960+kashkostwebsites@users.noreply.github.com>
2024-05-29 15:17:55 +0100

    filejs

--- file.js
+++ file.js
@@ -1,1 +1,10 @@
 const fs = require('fs');
+
+
+fs.appendFile('uifile1.ui', '<!-- Write some HTML code, We style it, No doctype html. -->',   (err)); {
+if (err) {
+  console.log('Failed adding UI file')
+}
+
+
+} 
